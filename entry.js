ff23b464c01da46832924af8dfee31a7aba79202
Jesse Bryan <winneonsword@gmail.com>
2016-09-25 03:20:14 -0500

    Redid entry.js.

--- entry.js
+++ entry.js
@@ -1,11 +1,14 @@
 'use strict'
 
-require('source-map-support').install()
-
-// ES6
+// ES6 Setup
+// import SourceMap from 'source-map-install'
 // import Main from './src/Main'
-// new Main()
+// SourceMap.install()
 
-// ES5
+// ES5 Setup
+const SourceMap = require('source-map-support')
 const Main = require('./lib/Main').default
+SourceMap.install()
+
+// Entry point
 new Main()
